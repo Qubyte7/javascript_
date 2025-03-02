c8fef57eb881c77dfa5c29086f247660356e0a97
Qubyte7 <shamiinnocent123@gmail.com>
2025-03-02 04:19:13 -0800

    first project commit

---
+++ react_lesson_1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h2 className="bg-blue-500 text-white ">
+
+            </h2>
+
+        </div>
+    )
+}
+export default App
