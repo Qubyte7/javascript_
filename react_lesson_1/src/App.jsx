4d498456d7a2f7eec6281b7d307219b66d830561
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2025-03-03 07:31:51 +0200

    commit

---
+++ react_lesson_1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h2 className="bg-blue-500 text-white ">
+            <h1 className="">SABRINA ndamwemera</h1>
+            </h2>
+
+        </div>
+    )
+}
+export default App
