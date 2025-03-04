cc649eb3f762910df3abe15ff68f4f1ab55a8e7c
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2025-03-04 13:45:22 +0200

    making search functionality

--- react_lesson_1/src/App.jsx
+++ react_lesson_1/src/App.jsx
@@ -18,12 +18,13 @@ const App = () => {
     const [movieList, setMovieList] = useState([]);
     const [Loading, setLoading] = useState(false);
 
-    const fetchMovies = async () => {
+    const fetchMovies = async (query = '') => {
         // while we are in the process of fetching we can do this
         setLoading(true);
         setErrorMessage('')
         try{
-            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            // the encodeURI we are using allow us to process our string in the Url
+            const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURI(query)}`: `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
             const response = await fetch(endpoint,API_OPTIONS)
             if(!response.ok){
                 throw new Error("Could not fetch movies from API");
@@ -47,8 +48,8 @@ const App = () => {
     }
     // we are calling the fetchmovies() function only at our first page load
     useEffect(() => {
-        fetchMovies()
-    },[])
+        fetchMovies(searchTerm)
+    },[searchTerm])
 
     return (
        <main>
