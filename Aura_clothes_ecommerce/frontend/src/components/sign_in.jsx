86018de6b478b00cc7b3740a512e4a46e1e49561
Qubyte7 <shamiinnocent123@gmail.com>
2025-03-10 16:07:04 +0000

    done with the authentication page

--- Aura_clothes_ecommerce/frontend/src/components/sign_in.jsx
+++ Aura_clothes_ecommerce/frontend/src/components/sign_in.jsx
@@ -1,15 +1,44 @@
 import React from 'react'
 import '../App.css'
+import { FaArrowRightLong } from "react-icons/fa6";
+
 
 const SignIn = () => {
     return (
         <div className="auth_page">
-            <div className="login">
-                Login
+            <div className="login_page">
+                <div className="flex flex-row gap-15">
+                    <img src="/images/logo_black.svg"  alt="aura logo"/>
+                    <h1 className='text-7xl font-display-crash-medium'>Aura</h1>
+                </div>
+                <p className='font-poppins text-4xl'>Login</p>
+                <div className="auth_form">
+                    <form className="flex flex-col gap-5">
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <p className='flex align-middle justify-center '>or</p>
+                        <button className='wide-btn-black-shadow flex justify-center '><img src="/images/google.svg" alt='google-logo'/></button>
+                        <button className=" wide-btn bg-black text-white font-lexend text-[1.2rem]">Login</button>
+                        <p className='font-lexend font-light flex justify-center flex-row gap-2'>Don't have an account ? <strong>Signup</strong> <FaArrowRightLong/></p>
+                    </form>
+                </div>
             </div>
 
-            <div className="signup bg-black text-white">
-                Signup
+            <div className="signup_page">
+            <div className="flex flex-row gap-15">
+                    <img src="/images/logo_white.svg"  alt="aura logo"/>
+                    <h1 className='text-7xl font-display-crash-medium'>Aura</h1>
+                </div>
+                <p className='font-poppins text-4xl'>Signup</p>
+                <div className="auth_form">
+                    <form className="flex flex-col gap-5">
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <p className='flex align-middle justify-center '>or</p>
+                        <button className='wide-btn-white-shadow  flex justify-center '><img src="/images/google.svg" alt='google-logo'/></button>
+                        <button className=" wide-btn bg-white text-black font-lexend text-[1.2rem]">Signup</button>
+                    </form>
+                </div>
             </div>
         </div>
     )
