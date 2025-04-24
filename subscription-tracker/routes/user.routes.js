bd0f45de0b5d9a935e0753de48f91c672bde2527
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2025-04-24 11:26:24 +0200

    autharizing users with token to make requests

--- subscription-tracker/routes/user.routes.js
+++ subscription-tracker/routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import  { getUsers, getUser} from "../controllers/user.controller.js"
+import authorize from "../middleware/auth.middlware.js";
 
 const userRouter =  Router();
 
-userRouter.get('/',(req,res)=>res.send({message: " User's routes"}))
-userRouter.get('/:id',(req,res)=>res.send({message: " a User's routes"}))
+userRouter.get('/',getUsers)
+userRouter.get('/:id',authorize,getUser)
 userRouter.post('/',(req,res)=>res.send({message: "creating a new user"}))
 userRouter.put('/:id',(req,res)=>res.send({message: " Updating a user"}))
 userRouter.delete('/:id',(req,res)=>res.send({message: " deleting a user"}))
