8e2da22f7da6f6a6ad75847614c8957f67961d46
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2025-04-26 08:51:14 +0200

    create first user

---
+++ Prisma/script.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "./generated/prisma";
+
+const prisma =  new PrismaClient();
+
+const main = async()=>{
+
+    const user = await prisma.user.create({
+        data:{
+            name:"Innocent",
+            email:"innocent@gmail.com",
+        }
+    });
+    console.log(user);
+    
+
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
